63a8a7f99e2fcaee184bbfe7553b1cb48eb3103e
Jay Vigilla <JayRVigilla@gmail.com>
2020-08-26 22:25:51 -0700

    WIP player functions
    began adding eyes to croc.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const CHOICES = 12;
+let round = null;
 
 function randomNumber(choices) {
   return Math.ceil((Math.random()*choices))
 }
 let trigger = randomNumber(CHOICES);
+console.log("*trigger*",trigger);
 
 function isTrigger() {
     if (confirm("CHOMP !!!\nNew Game?")) resetBoard();
@@ -13,7 +15,7 @@ const teeth = document.querySelectorAll('.tooth');
 function resetBoard() {
   teeth.forEach(t => t.setAttribute('picked', 'false'));
   trigger = randomNumber(CHOICES);
-  console.log(trigger);
+  console.log("*trigger*",trigger);
 }
 
 /**
@@ -21,24 +23,48 @@ function resetBoard() {
  * event listener on click in jaw
  *    if div.tooth has attribute picked === 'false' -> change to 'true'
  */
-const jaw = document.querySelector('#jaw');
+const jaw = document.querySelector('#lower-jaw');
 jaw.addEventListener("click", pickTooth);
 
 function togglePicked(t) {
   if (t.getAttribute('picked') === 'false') t.setAttribute('picked', 'true');
+  incrementTurn();
+  console.log(decideTurn(),"**", turnCounter)
 }
 
 function pickTooth(evt) {
   const tooth = evt.target;
   const toothNum = tooth.getAttribute('num');
-  console.log('*****\n\n Value of toothNum in ', toothNum, '\n\n *****')
+  console.log("#", toothNum);
   parseInt(toothNum) === trigger ? isTrigger() : togglePicked(tooth);
 }
 
-console.log(trigger);
-
 /**
  * Players
- * name player 1 & 2
- * track turns
+ * choose amount of players:
+ *    game operates under musical chairs rules -> loser out, rest continue until one remaining
+ *    dynamically add player spaces to message hub
+ * name players
+ * track whose turn
+ * upon losing remove losing player
  */
+let playerCount = 0;
+let turnCounter = 1;
+let players = {};
+
+function setPlayerCount(num) {
+  if (round === null) askHowManyPlayers();
+  playerCount = num;
+}
+
+function incrementTurn() {
+  turnCounter += 1
+}
+
+function askHowManyPlayers() {
+
+}
+
+function decideTurn() {
+  return(turnCounter % 2 === 0 ? "Player 2" : "Player 1");
+}
